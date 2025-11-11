87d336576f70f1df3d725f412dc8c738a3b572fd
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2025-11-11 14:34:50 +0100

    Clarify version info in PDF

--- validation-stacie-report/plan.py
+++ validation-stacie-report/plan.py
@@ -9,8 +9,8 @@ from stepup.reprep.api import compile_typst, wrap_git
 
 # Write Git information to text file for inclusion in documents.
 glob("../.git/**", _defer=True)
-wrap_git("git describe --tags --long", out="git-version.txt")
-wrap_git("git log -n1 --pretty='format:(%cs)'", out="git-date.txt")
+wrap_git("git describe --tags", out="git-version.txt")
+wrap_git("git log -n1 --pretty='format:(%cs %h)'", out="git-date.txt")
 
 static("../validation-stacie-calc/")
 glob("../validation-stacie-calc/output/**", _defer=True)
