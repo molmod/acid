899da2c9c0ea4c8c53bf9ff83071600ac2f8db7d
RobbeBohy <robbe.bohy@ugent.be>
2026-04-15 14:16:49 +0200

    Resolve singularity + introduce new variables

--- 1_dataset/lib/kernels.py
+++ 1_dataset/lib/kernels.py
@@ -68,19 +68,19 @@ class SHOTerm(BaseTerm):
         q = self.q
         eta = np.sqrt(abs(1 / (4 * q**2) - 1))
         ft = 2 * np.pi * f0 * abs(times)
-        prefactor = a0 * (1 - q**2) / (2 * np.pi * f0 * q)
-        scarg = eta * ft
+        prefactor = a0 / (2 * np.pi * f0 * q)
 
         if 0 < q < 0.5:
-            acf = q * (np.exp((eta - 0.5 / q) * ft) + np.exp((-eta - 0.5 / q) * ft))
-            acf += (np.exp((eta - 0.5 / q) * ft) - np.exp((-eta - 0.5 / q) * ft)) / (2 * eta)
+            exp_arg_plus = (eta - 0.5 / q) * ft
+            exp_arg_minus = (-eta - 0.5 / q) * ft
+            sinh_prefactor = 1 / (2 * eta * q) * (1 - 3 * q**2)
+
+            acf = q * (np.exp(exp_arg_plus) + np.exp(exp_arg_minus))
+            acf += (np.exp(exp_arg_plus) - np.exp(exp_arg_minus)) / (2 * eta)
             acf *= 0.5 * np.pi * a0 * f0
 
-            sinsinh_prefactor = 1 / (2 * eta * q) * ((1 - 3 * q**2) / (1 - q**2))
-            msd = np.exp(scarg - ft / (2 * q)) + np.exp(-scarg - ft / (2 * q))
-            msd += sinsinh_prefactor * (
-                np.exp(scarg - ft / (2 * q)) - np.exp(-scarg - ft / (2 * q))
-            )
+            msd = (1 - q**2) * (np.exp(exp_arg_plus) + np.exp(exp_arg_minus))
+            msd += sinh_prefactor * (np.exp(exp_arg_plus) - np.exp(exp_arg_minus))
             msd *= prefactor / 2
 
         elif q >= 0.5:
@@ -89,14 +89,16 @@ class SHOTerm(BaseTerm):
             if q == 0.5:
                 acf *= 1 + ft
                 msd *= 1 + 2 / 3 * np.pi * f0 * abs(times)
+                msd *= 1 - q**2
             else:
-                sinsinh_prefactor = 1 / (2 * eta * q) * ((1 - 3 * q**2) / (1 - q**2))
+                scarg = eta * ft
+                sin_prefactor = 1 / (2 * eta * q) * (1 - 3 * q**2)
                 acf *= np.cos(scarg) + np.sin(scarg) / (2 * eta * q)
-                msd *= np.cos(scarg) + sinsinh_prefactor * np.sin(scarg)
+                msd *= (1 - q**2) * np.cos(scarg) + sin_prefactor * np.sin(scarg)
         else:
             raise ValueError(f"Invalid {q=}")
 
-        msd += a0 * abs(times) - prefactor
+        msd += a0 * abs(times) - prefactor * (1 - q**2)
         psd = a0 * f0**4 / ((freqs**2 - f0**2) ** 2 + (freqs * f0 / q) ** 2)
         return acf, psd, msd
 
