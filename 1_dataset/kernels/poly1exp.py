19c1e7153818f0ff410fab344aa8a7c974661a86
RobbeBohy <robbe.bohy@ugent.be>
2026-04-20 11:33:14 +0200

    Propose new set of kernels

--- 1_dataset/kernels/poly1exp.py
+++ 1_dataset/kernels/poly1exp.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: © 2026 ACID Contributors <https://doi.org/10.5281/zenodo.15722902>
 # SPDX-License-Identifier: CC-BY-SA-4.0 OR LGPL-3.0-or-later
 
-from kernels import SHOTerm, WhiteTerm
+from kernels import ExpTerm, PolyTerm
 
-terms = [SHOTerm(0.9, 0.15, 0.2), WhiteTerm(0.1)]
+terms = [PolyTerm(0.2, 1.5, 5.0), ExpTerm(0.8, 2.0)]
--- 1_dataset/kernels/poly1w.py
+++ 1_dataset/kernels/poly1w.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: © 2026 ACID Contributors <https://doi.org/10.5281/zenodo.15722902>
 # SPDX-License-Identifier: CC-BY-SA-4.0 OR LGPL-3.0-or-later
 
-from kernels import SHOTerm
+from kernels import PolyTerm, WhiteTerm
 
-terms = [SHOTerm(0.8, 0.15, 0.3), SHOTerm(0.2, 0.35, 0.1)]
+terms = [PolyTerm(0.9, 1.5, 5.0), WhiteTerm(0.1)]
